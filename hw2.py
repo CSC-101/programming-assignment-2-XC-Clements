8681c5f743db5027575a62e3e8f10aee0827dfe2
Hikatchus <76414030+Hikatchus@users.noreply.github.com>
2024-10-28 10:01:01 -0700

    first com

--- hw2.py
+++ hw2.py
@@ -1,17 +1,72 @@
 import data
+from data import Point, Rectangle, Duration, Song
 
+def total_duration(time:Duration):
+    output_duration = (time.minutes * 60) + time.seconds
+    return output_duration
+def time_to_duration(time:int):
+    output_duration = Duration((time // 60), (time % 60))
+    return output_duration
 # Write your functions for each part in the space below.
 
 # Part 1
+#takes two input points in any order, and returns a Rectangle that is properly formatted
+def create_rectangle(point_a:Point, point_b:Point):
+    top_left = Point(0, 0)
+    bottom_right = Point(0,0)
+    if point_a.x < point_b.x:
+        bottom_right.x = point_b.x
+        top_left.x = point_a.x
+    else:
+        bottom_right.x = point_a.x
+        top_left.x = point_b.x
+    if point_a.y > point_b.y:
+        top_left.y = point_a.y
+        bottom_right.y = point_b.y
+    else:
+        bottom_right.y = point_a.y
+        top_left.y = point_b.y
+    return Rectangle(top_left, bottom_right)
 
+print(create_rectangle(Point(0, 0), Point(10, 10)))
 
 # Part 2
+#Takes two Durations, and if the first duration is less than the second returns True else False
+def shorter_duration_than(dur_a:Duration, dur_b:Duration):
+    dur_a_tot = (dur_a.minutes * 60) + dur_a.seconds
+    dur_b_tot = (dur_b.minutes * 60) + dur_b.seconds
+    if dur_a_tot < dur_b_tot:
+        return True
+    else:
+        return False
+
 
 
 # Part 3
+#takes a list of songs, and returns the list of songs that are under the given duration
+def song_shorter_than(song_list:list[Song], max_dur:Duration):
+    output_list = []
+    for n in song_list:
+        if total_duration(n.duration) < total_duration(max_dur):
+            output_list.append(n)
+    return output_list
 
+list_of_songs = [Song("Earth, Wind & Fire", "September", Duration(3, 35)),
+                 Song("Jack Stauber's Micropop", "Baby Hotline", Duration(4, 51)),
+                 Song("Kesha", "Blow", Duration(3,39)),
+                 Song("Fountains of Wayne", "Stacy's Mom", Duration(3, 17))]
+print(song_shorter_than(list_of_songs, Duration(4, 0)))
 
 # Part 4
+#takes an input song list, and a list of which indices to take songs from, compiles them into a list and adds together the total duration of the new playlist
+def running_time(input_song_list:list[Song], playlist_numbers:list[int]):
+    output_time = total_duration(Duration(0,0))
+    for n in playlist_numbers:
+        output_time += total_duration(input_song_list[n].duration)
+    output_duration = time_to_duration(output_time)
+    return output_duration
+
+print(running_time(list_of_songs, [0,1,1,3,2]))
 
 
 # Part 5
--- hw2_tests.py
+++ hw2_tests.py
@@ -2,19 +2,34 @@ import data
 import hw2
 import unittest
 
+from data import Point, Rectangle, Duration
+from hw2 import create_rectangle, shorter_duration_than, running_time, list_of_songs
+
 
 # Write your test cases for each part below.
 
 class TestCases(unittest.TestCase):
     # Part 1
-
-
+    def test_create_rectangle_1(self):
+        output_rect = create_rectangle(Point(0,0), Point(10, 10))
+        self.assertEqual(output_rect, Rectangle(Point(0, 10), Point(10, 0)))
+    def test_create_rectangle_2(self):
+        output_rect = create_rectangle(Point(-4,15), Point(10, 10))
+        self.assertEqual(output_rect, Rectangle(Point(-4, 15), Point(10, 10)))
     # Part 2
-
-
+    def test_shorter_duration_than_1(self):
+        dur_1 = Duration(10, 5)
+        dur_2 = Duration(15, 27)
+        self.assertEqual(shorter_duration_than(dur_1,dur_2), True)
+    def test_shorter_duration_than_2(self):
+        dur_1 = Duration(4, 107)
+        dur_2 = Duration(5, 20)
+        self.assertEqual(shorter_duration_than(dur_1,dur_2), False)
     # Part 3
-
-
+    def test_running_time_1(self):
+        self.assertEqual(running_time(list_of_songs, [0, 1, 1, 3, 2]), Duration(20, 13))
+    def test_running_time_2(self):
+        self.assertEqual(running_time(list_of_songs, [0]), Duration(3, 35))
     # Part 4
 
 
